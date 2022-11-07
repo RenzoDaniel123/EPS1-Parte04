a28355fbe81c49bdcaca155fcfb94a84187454d8
RenzoDaniel123 <20a3100045@untels.edu.pe>
2022-11-07 10:13:12 -0500

    Parte 1 Realizado por Felix

---
+++ Aplicacion.py
@@ -0,0 +1,9 @@
+
+print("\tBienvenido al programa")
+print("    Menú")
+print("1. Listar alumno")
+print("2. Agregar alumnos")
+print("3. Eliminar alumnos")
+print("4. Salir")
+
+opcion = int(input("Seleccione una opción: "))
