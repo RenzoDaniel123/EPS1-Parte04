70ffb2ddbcbc7f028439fa1d24e8e44cb416868e
Maycoll01 <20a3010090@untels.edu.pe>
2022-11-07 10:39:26 -0500

    Parte2 realizado por Escobar

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,3 +1,16 @@
+import sqlite3
+conexion = sqlite3.connect("EscobarMejia_almacen.db")
+tabla_Producto="""CREATE TABLE Producto(
+                idproducto INTEGER PRIMARY KEY AUTOINCREMENT,
+                codigo TEXT UNIQUE,
+                nombre VARCHAR(20),
+                precio REAL    
+                )
+               """
+cursor = conexion.cursor()
+cursor.execute(tabla_Producto)
+conexion.close()
+
 
 print("\tBienvenido al programa")
 print("    Menú")
@@ -7,3 +20,4 @@ print("3. Eliminar alumnos")
 print("4. Salir")
 
 opcion = int(input("Seleccione una opción: "))
+
