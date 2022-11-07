fafd9ea9f18f799a3f4269faa09c7a8180d38977
RenzoDaniel123 <20a3100045@untels.edu.pe>
2022-11-07 12:02:04 -0500

    Parte 4 Realizado por Felix

--- Aplicacion.py
+++ Aplicacion.py
@@ -12,28 +12,6 @@ conexion = sqlite3.connect("EscobarMejia_almacen.db")
 #cursor.execute(tabla_Producto)
 #conexion.close()
 
-lista_productos = [('001','Televisor Smart 39',749),
-                 ('002','Redmi 10C 3G+64GB',599),                
-                 ('003','Televisor 70 LG UHD',2899),
-                 ('004','Sansung A53 5G',1449),
-                 ('005','Galaxy A23 128GB 4GB',799),
-                 ('006','MOTO G31 4GB + 128GB',949),
-                 ('007','Lavadora 16Kg Blanco',1299),
-                 ('008','PS5 HW STANDARD',4299),
-                 ('009','Lavadora Inverter',1699),
-                 ('010','Control PS4 negro',329)
-                 ]
- 
-consulta_registro = """INSERT INTO
-                              Producto (codigo, nombre, precio)
-                              VALUES (?,?,?)
-                            """
-cursor = conexion.cursor()
-cursor.executemany(consulta_registro, lista_productos)
-conexion.commit()
-conexion.close()
-
-
 
 cursor = conexion.cursor()
 print("\tBienvenido al programa")
@@ -63,16 +41,29 @@ if opcion == 1:
     conexion.commit()
     conexion.close()
 
+if opcion == 2:
+    print("\tELIMINAR PRODUCTO")
+    print("Ingrese código")
+    codigo = input()
+    consulta_codigo = """ DELETE FROM 
+                          Producto 
+                          WHERE
+                          CODIGO =?""" 
+    cursor.execute(consulta_codigo,(codigo,))
+    conexion.commit()
+    conexion.close()
+    
 if opcion == 4:
     consulta_lista = """SELECT *
                             FROM Producto
                             ORDER BY codigo
                               """
+   
+
     cursor.execute(consulta_lista)
     lista = cursor.fetchall()
     for Producto in lista:
         print(Producto)
 
-
-
-
+if opcion==5:
+    print("Finalizando el programa")
